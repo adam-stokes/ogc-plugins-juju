4378f7df6183e1670dc6a3f1bfc10f1274cf4f24
Adam Stokes <battlemidget@users.noreply.github.com>
2019-07-16 17:36:57 -0400

    Add ability to bootstrap, deploy
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ogc_plugins_juju/__init__.py
+++ ogc_plugins_juju/__init__.py
@@ -4,33 +4,185 @@
 import os
 import click
 import sys
-
+import sh
+from pprint import pformat
 from ogc import log
+from ogc.spec import SpecPlugin
 
 
-class Juju:
-    """ OGC Env Plugin
+class Juju(SpecPlugin):
+    """ OGC Juju Plugin
 
-    [Env]
-    # Test plans require certain environment variables to be set prior to running.
-    # This module allows us to make sure those requirements are met before
-    # proceeding.
-    requires = ["CHARMCREDS", "JUJUCREDS"]
+    [Juju]
+    # Juju module for bootstrapping and deploying a bundle
+    cloud = "aws"
 
-    # Optionally, define a location of KEY=VALUE line items to use as this specs
-    # environment variables
-    properties-file = "/home/user/env.properties"
+    # controller to create
+    controller = "validator"
+
+    # model to create
+    model = "validator-model"
+
+    [Juju.bootstrap]
+    # turn on debugging
+    debug = false
+
+    # disable adding the specified model, usually when some configuration on the
+    # models have to be done
+    disable-add-model = true
+
+    [Juju.deploy]
+    # reuse existing controller/model
+    reuse = True
+
+    # bundle to deploy
+    # bundle = "cs:~owner/custom-bundle"
+    bundle = "bundles/my-custom-bundle.yaml"
+
+    # Optional overlay to pass into juju
+    overlay = "overlays/1.15-edge.yaml"
+
+    # Optional bundle channel to deploy from
+    bundle_channel = "edge"
+
+    # Optional charm channel to deploy from
+    charm_channel = "edge"
+
+    # Wait for a deployment to settle?
+    wait = true
+
+    [Juju.config]
+    # Config options to pass to a deployed application
+    # ie, juju config -m controller:model kubernetes-master allow-privileged=true
+    set = ["kubernetes-master = allow-privileged=true",
+           "kubernetes-worker = allow-privileged=true"]
     """
 
-    VERSION = "0.0.1"
+    NAME = "Juju Plugin"
 
-    def process(self, spec):
-        """ Processes env options
+    deps = ["snap:juju", "snap:charm"]
+
+    options = [
+        ("cloud", True),
+        ("controller", True),
+        ("model", True),
+        ("bootstrap", True),
+        ("bootstrap.constraints", False),
+        ("bootstrap.debug", False),
+        ("bootstrap.disable_add_model", False),
+        ("deploy", False),
+        ("deploy.reuse", False),
+        ("deploy.bundle", True),
+        ("deploy.overlay", False),
+        ("deploy.bundle_channel", False),
+        ("deploy.charm_channel", False),
+        ("deploy.wait", False),
+        ("config", False),
+        ("config.set", False),
+    ]
+
+    @property
+    def _fmt_controller_model(self):
+        return f"{self.get_option('controller')}:{self.get_option('model')}"
+
+    def _deploy(self):
+        """ Handles juju deploy
         """
-        env = os.environ.copy()
-        check_requires = spec.get('requires', None)
-        if check_requires and not set(check_requires) < set(env):
-            env_differ = ", ".join(list(set(check_requires).difference(env)))
-            log.debug(f"Requirements {env_differ} not found in host environment")
+        bundle = self.get_option("deploy.bundle")
+        overlay = self.get_option("deploy.overlay")
+        bundle_channel = self.get_option("deploy.bundle_channel")
+        charm_channel = self.get_option("deploy.charm_channel")
+
+        deploy_cmd_args = []
+        charm_pull_args = []
+        if bundle.startswith("cs:"):
+            charm_pull_args.append(bundle)
+            if bundle_channel:
+                charm_pull_args.append("--channel")
+                charm_pull_args.append(bundle_channel)
+                charm_pull_args.append("./bundle-to-test")
+            # Access charmstore bundle
+            sh.charm.pull(bundle, *charm_pull_args)
+            deploy_cmd_args = [
+                "-m",
+                self._fmt_controller_model,
+                "./bundle-to-test/bundle.yaml",
+            ]
+            if overlay:
+                deploy_cmd_args.append("--overlay")
+                deploy_cmd_args.append(overlay)
+            if charm_channel:
+                deploy_cmd_args.append("--channel")
+                deploy_cmd_args.append(charm_channel)
+            for line in sh.juju.deploy(*deploy_cmd_args, _iter=True, _err_to_out=True):
+                line = line.strip()
+                log.info(line)
+        else:
+            deploy_cmd_args = ["-m", self._fmt_controller_model, bundle]
+            for line in sh.juju.deploy(*deploy_cmd_args, _iter=True, _err_to_out=True):
+                line = line.strip()
+                log.info(line)
+
+    def _bootstrap(self):
+        """ Bootstraps environment
+        """
+        bootstrap_cmd_args = [
+            "bootstrap",
+            self.get_option("cloud"),
+            self.get_option("controller"),
+        ]
+        bootstrap_constraints = self.get_option("bootstrap.constraints")
+        if bootstrap_constraints:
+            bootstrap_cmd_args.append("--bootstrap-constraints")
+            bootstrap_cmd_args.append(bootstrap_constraints)
+
+        bootstrap_debug = self.get_option("bootstrap.debug")
+        if bootstrap_debug:
+            bootstrap_cmd_args.append("--debug")
+        try:
+            for line in sh.juju(*bootstrap_cmd_args, _iter=True, _err_to_out=True):
+                line = line.strip()
+                log.debug(line)
+        except sh.ErrorReturnCode_1 as e:
+            log.error(f"Unable to bootstrap:\n {e.stdout.decode()}")
             sys.exit(1)
-        return
+
+        disable_add_model = self.get_option("bootstrap.disable_add_model")
+        if not disable_add_model:
+            log.info(f"Adding model {self._fmt_controller_model}")
+            add_model_args = [
+                "-c",
+                self.get_option("controller"),
+                self.get_option("model"),
+                self.get_option("cloud"),
+            ]
+
+            sh.juju("add-model", *add_model_args)
+
+    def _wait(self):
+        deploy_wait = (
+            self.get_option("deploy.wait") if self.get_option("deploy.wait") else False
+        )
+        if deploy_wait:
+            log.info("Waiting for deployment to settle")
+            log.debug(
+                sh.juju_wait("-e", self._fmt_controller_model, "-w", "-r3", "-t14400")
+            )
+
+    def process(self):
+        """ Processes options
+        """
+        # Bootstrap unless reuse is true, controller and model must exist already
+        if not self.get_option("deploy.reuse"):
+            self._bootstrap()
+
+        # Do deploy
+        if self.get_option("deploy"):
+            self._deploy()
+            self._wait()
+            config_sets = self.get_option("config.set")
+            if config_sets:
+                for config in config_sets:
+                    log.info(f"Setting {config}")
+                    sh.juju.config("-m", self._fmt_controller_model, config)
+            self._wait()
--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 import setuptools
 
-import ogc_plugins_juju
-
 setuptools.setup(
     name="ogc-plugins-juju",
-    version=ogc_plugins_env.Juju.VERSION,
+    version="0.0.1",
     author="Adam Stokes",
     author_email="adam.stokes@ubuntu.com",
     description="ogc-plugins-juju, a ogc plugin for working with juju",
     url="https://github.com/battlemidget/ogc-plugins-juju",
-    packages=['ogc_plugins_juju'],
-    entry_points={
-        "ogc.plugins": 'Juju = ogc_plugins_juju:Juju'
-    },
-    install_requires = [
-        'ogc>=0.1.5,<1.0.0',
-        'click>=7.0.0,<8.0.0',
-    ]
+    packages=["ogc_plugins_juju"],
+    entry_points={"ogc.plugins": "Juju = ogc_plugins_juju:Juju"},
+    install_requires=[
+        "ogc>=0.1.5,<1.0.0",
+        "click>=7.0.0,<8.0.0",
+        "sh>=1.12,<2.0",
+        "juju-wait==2.7.0",
+    ],
 )
