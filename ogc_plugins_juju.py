5b0f086e17d5bcf51ae95d1dc7f3759e29af38c5
Adam Stokes <battlemidget@users.noreply.github.com>
2019-08-27 15:01:53 -0400

    update readme
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ogc_plugins_juju.py
+++ ogc_plugins_juju.py
@@ -18,44 +18,62 @@ __maintainer_email__ = "adam.stokes@gmail.com"
 __description__ = "ogc-plugins-juju, a ogc plugin for working with juju"
 __git_repo__ = "https://github.com/battlemidget/ogc-plugins-juju"
 __example__ = """
-setup:
-  - juju:
-      cloud: $JUJU_CLOUD
-      controller: $JUJU_CONTROLLER
-      model: $JUJU_MODEL
-      - bootstrap:
-          debug: no
-          model-default: test-mode=true
-      - deploy:
-          bundle: charmed-kubernetes
-          overlay: |
-            applications:
-              kubernetes-master:
-                options:
-                  channel: $SNAP_VERSION
-              kubernetes-worker:
-                options:
-                  channel: $SNAP_VERSION
-          wait: yes
-      - config:
-          - kubernetes-master allow-privileged=true
-          - kubernetes-worker allow-privileged=true
+meta:
+  name: Validate Charmed Kubernetes
+  description: |
+    Runs validation test suite against a vanilla deployment of Charmed Kubernetes
 
 plan:
-  - runner:
-      description: "Full validation of charmed kubernetes"
-      fail-silently: yes
-      script: |
+  - &BASE_JOB
+    env:
+      - SNAP_VERSION=1.16/edge
+      - JUJU_DEPLOY_BUNDLE=cs:~containers/charmed-kubernetes
+      - JUJU_DEPLOY_CHANNEL=edge
+      - JUJU_CLOUD=aws/us-east-2
+      - JUJU_CONTROLLER=validate-ck
+      - JUJU_MODEL=validate-model
+    install:
+      - pip install -rrequirements.txt
+      - pip install -rrequirements_test.txt
+      - pip install git+https://github.com/juju/juju-crashdump.git
+      - sudo apt install -qyf build-essential
+      - sudo snap install charm --edge --classic
+      - sudo snap install juju --classic
+      - sudo snap install aws-cli --classic
+    before-script:
+      - juju:
+          cloud: $JUJU_CLOUD
+          controller: $JUJU_CONTROLLER
+          model: $JUJU_MODEL
+          bootstrap:
+            debug: no
+            replace-controller: yes
+            model-default:
+              - test-mode=true
+          deploy:
+            reuse: yes
+            bundle: $JUJU_DEPLOY_BUNDLE
+            overlay: |
+              applications:
+                kubernetes-master:
+                  options:
+                    channel: $SNAP_VERSION
+                kubernetes-worker:
+                  options:
+                    channel: $SNAP_VERSION
+            wait: yes
+            channel: $JUJU_DEPLOY_CHANNEL
+    script:
+      - |
         #!/bin/bash
-        pytest validations/tests/validation.py \
-           --connection $JUJU_CONTROLLER:$JUJU_MODEL \
-           --cloud $JUJU_CLOUD \
-           --bunndle-channel $JUJU_DEPLOY_CHANNEL \
-           --snap-channel $SNAP_VERSION
-teardown:
-  - runner:
-      description: Destroy juju environment, cleanup storage
-      cmd: juju destroy-controller -y --destroy-all-models --destroy-storage $JUJU_CONTROLLER
+        set -eux
+        pytest jobs/integration/validation.py \
+             --cloud $JUJU_CLOUD \
+             --controller $JUJU_CONTROLLER \
+             --model $JUJU_MODEL
+    after-script:
+      - juju-crashdump -a debug-layer -a config -m $JUJU_CONTROLLER:$JUJU_MODEL
+      - juju destroy-controller -y --destroy-all-models --destroy-storage $JUJU_CONTROLLER
 """
 
 
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ juju plugin for ogc
 
 # usage
 
-In a ogc spec, place the following in whatever phase (setup, plan, teardown):
+In a ogc spec, place the following in your plan:
 
 ```yaml
 meta:
@@ -12,44 +12,55 @@ meta:
   description: |
     Runs validation test suite against a vanilla deployment of Charmed Kubernetes
 
-setup:
-  - juju:
-      - bootstrap:
-          debug: no
-          model-default: test-mode=true
-      - deploy:
-          bundle: charmed-kubernetes
-          overlay: |
-            applications:
-              kubernetes-master:
-                options:
-                  channel: $SNAP_VERSION
-              kubernetes-worker:
-                options:
-                  channel: $SNAP_VERSION
+plan:
+  - &BASE_JOB
+    env:
+      - SNAP_VERSION=1.16/edge
+      - JUJU_DEPLOY_BUNDLE=cs:~containers/charmed-kubernetes
+      - JUJU_DEPLOY_CHANNEL=edge
+      - JUJU_CLOUD=aws/us-east-2
+      - JUJU_CONTROLLER=validate-ck
+      - JUJU_MODEL=validate-model
+    install:
+      - pip install -rrequirements.txt
+      - pip install -rrequirements_test.txt
+      - pip install git+https://github.com/juju/juju-crashdump.git
+      - sudo apt install -qyf build-essential
+      - sudo snap install charm --edge --classic
+      - sudo snap install juju --classic
+      - sudo snap install aws-cli --classic
+    before-script:
+      - juju:
           cloud: $JUJU_CLOUD
           controller: $JUJU_CONTROLLER
           model: $JUJU_MODEL
-          wait: yes
-      - config:
-          - kubernetes-master allow-privileged=true
-          - kubernetes-worker allow-privileged=true
-
-plan:
-  - runner:
-      description: "Full validation of charmed kubernetes"
-      fail-silently: yes
-      script: |
+          bootstrap:
+            debug: no
+            replace-controller: yes
+            model-default:
+              - test-mode=true
+          deploy:
+            reuse: yes
+            bundle: $JUJU_DEPLOY_BUNDLE
+            overlay: |
+              applications:
+                kubernetes-master:
+                  options:
+                    channel: $SNAP_VERSION
+                kubernetes-worker:
+                  options:
+                    channel: $SNAP_VERSION
+            wait: yes
+            channel: $JUJU_DEPLOY_CHANNEL
+    script:
+      - |
         #!/bin/bash
-        pytest validations/tests/validation.py \
-           --connection $JUJU_CONTROLLER:$JUJU_MODEL \
-           --cloud $JUJU_CLOUD \
-           --bunndle-channel $JUJU_DEPLOY_CHANNEL \
-           --snap-channel $SNAP_VERSION
-teardown:
-  - runner:
-      description: Destroy juju environment, cleanup storage
-      cmd: juju destroy-controller -y --destroy-all-models --destroy-storage $JUJU_CONTROLLER
+        set -eux
+        pytest jobs/integration/validation.py \
+             --cloud $JUJU_CLOUD \
+             --controller $JUJU_CONTROLLER \
+             --model $JUJU_MODEL
+    after-script:
+      - juju-crashdump -a debug-layer -a config -m $JUJU_CONTROLLER:$JUJU_MODEL
+      - juju destroy-controller -y --destroy-all-models --destroy-storage $JUJU_CONTROLLER
 ```
-
-### see `ogc spec-doc Juju` for more information.
