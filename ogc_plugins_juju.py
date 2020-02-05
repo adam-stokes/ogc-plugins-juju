156b84cff037d43184b6a2cc6024a811fc6de078
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-05 13:01:01 -0500

    Version updated from 1.0.23 to 1.0.24

--- ogc_plugins_juju.py
+++ ogc_plugins_juju.py
@@ -13,7 +13,7 @@ from ogc.spec import SpecPlugin
 from ogc.state import app
 
 __plugin_name__ = "ogc-plugins-juju"
-__version__ = "1.0.23"
+__version__ = "1.0.24"
 __author__ = "Adam Stokes"
 __author_email__ = "adam.stokes@gmail.com"
 __maintainer__ = "Adam Stokes"
