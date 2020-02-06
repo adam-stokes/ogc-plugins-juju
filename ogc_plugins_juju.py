4a41e5035cda1a8c5fd24e514bf1940d9d1420f5
Adam Stokes <battlemidget@users.noreply.github.com>
2020-02-06 12:58:38 -0500

    more syntax
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- ogc_plugins_juju.py
+++ ogc_plugins_juju.py
@@ -310,7 +310,7 @@ class Juju(SpecPlugin):
             deploy_cmd_args.append(channel)
         if constraints:
             deploy_cmd_args.append("--constraints")
-            deploy_cmd_args.append(f'"{constraints}"')
+            deploy_cmd_args.append(constraints)
         if series:
             deploy_cmd_args.append("--series")
             deploy_cmd_args.append(series)
